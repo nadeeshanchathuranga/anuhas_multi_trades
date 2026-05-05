4fda3be34acbc45c34cdf544084288ff6db21a48
PawanMP <pasindupawan7579@gmail.com>
2026-05-05 14:32:52 +0530

    refactor: add peer dependencies and update server configuration in package-lock.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            valetTls: false,
             refresh: true,
         }),
         vue({
