6090cda07d53e51775514d4c5cf3d2aa1af1b3b1
N.D.V.Ovinya Perera <windypereira39@gmail.com>
2026-06-19 13:58:46 +0530

    fix: remove size attribute and overflow style from order dropdown for improved display

--- resources/js/Pages/Pos/Index.vue
+++ resources/js/Pages/Pos/Index.vue
@@ -788,9 +788,7 @@
               <select
                 id="order_id"
                 v-model="ReturnbillForm.order_id"
-                class="mt-2 p-4 border border-gray-300 rounded-lg text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 max-h-64"
-                style="overflow-y: auto;"
-                size="8"
+                class="mt-2 p-4 border border-gray-300 rounded-lg text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               >
                 <option value="" disabled>Select an order</option>
